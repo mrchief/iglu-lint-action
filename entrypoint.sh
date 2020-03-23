fb0d42e77a9a732e7f71242939fbbefd96aa15de
Hrusikesh Panda <mrchief_2000@yahoo.com>
2020-03-22 20:32:29 -0400

    feat: Remove previous comments before posting a new one

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,29 @@ commitId=$(cat ${GITHUB_EVENT_PATH} | jq --raw-output .after)
 owner=$(cat ${GITHUB_EVENT_PATH} | jq --raw-output .repository.owner.login)
 repo=$(cat ${GITHUB_EVENT_PATH} | jq --raw-output .repository.name)
 
+allComments=$(curl -s "https://api.github.com/repos/${owner}/${repo}/pulls/${prNum}/comments" \
+    -H 'Accept: application/vnd.github.comfort-fade-preview+json' \
+    -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}")
+
 info() {
     if [[ ! -z "$VERBOSE" ]]; then
         echo -e "\n>>>>>>>\n"
         echo $@
         echo -e "\n<<<<<<<\n"
     fi
+}
+
+removePreviousComments() {
+    info "removing comments for: $1 $2"
+    echo $allComments |
+        jq --arg file "$1" --argjson line $2 '.[]
+          | select(.user.login=="github-actions[bot]")
+          | { id: .id, path: .path, line: .line }
+          | select(.path==$file and .line==$line)
+          | .id' |
+        xargs -r -I {} curl -X DELETE "https://api.github.com/repos/${owner}/${repo}/pulls/comments/{}" \
+            -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
+            ${VERBOSE:--S} ${VERBOSE:+-v} -s
 }
 
 postJsonLintError() {
@@ -53,6 +70,7 @@ postComment() {
 
     info "body: ${body}"
 
+    removePreviousComments "$1" "$3"
     curl "https://api.github.com/repos/${owner}/${repo}/pulls/${prNum}/comments" \
         -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
         -H 'Content-Type: application/json' \
